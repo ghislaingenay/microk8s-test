f3fef5a9c63ad3a53f927ad7d0668b9ac6a25268
ghislaingenay <ghislain.gny@gmail.com>
2024-05-16 11:36:06 +0700

    backend/feat:add cors + env system

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,53 @@
 import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { Context, Env, Hono } from "hono";
+import * as dotenv from "dotenv";
+import {cors} from "hono/cors";
 import { TaskCreate, taskManager } from "./TaskManager";
+import { BlankInput } from "hono/types";
+
+dotenv.config();
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  const tasks = taskManager.getTasks();
+app.use('*', cors({
+  origin: 'http://localhost:5173',
+  allowHeaders: ['Content-Type', 'Authorization'],
+  allowMethods: ['POST', 'GET', 'OPTIONS', 'DELETE'],
+  exposeHeaders: ['Content-Length'],
+  maxAge: 600,
+  credentials: true,
+}))
+
+const checkAuth = (c: Context<Env, "/", BlankInput>) => {
+  const accessToken = process.env.ACCESS_TOKEN
+  if (!accessToken) throw new Error("ACCESS_TOKEN is not set");
+  if (c.req.header("Authorization") !== `Bearer ${accessToken}`) {
+    throw new Error("Invalid auth is not set");
+  }}
+
+app.get("/tasks", (c) => {
+  checkAuth(c)
+  const tasks = taskManager.getTasks()
   return c.json(tasks);
 });
 
-app.post("/", async (c) => {
+app.post("/tasks", async (c) => {
+  checkAuth(c);
   const body = await c.req.json<TaskCreate>();
   taskManager.addTask(body);
   return c.json({ message: "Task added", data: body });
 });
 
-app.delete("/:id", (c) => {
+app.delete("/tasks/:id", (c) => {
+  checkAuth(c);
   const id = Number(c.req.param("id"));
   taskManager.deleteTask(id);
   return c.json({ message: "Task deleted" });
 });
 
-const port = 3000;
+const port = 8000;
+
+
 console.log(`Server is running on port ${port}`);
 
 serve({
