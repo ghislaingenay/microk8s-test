f265363a10e772c8c68320e107cd12c6c65d2192
ghislaingenay <ghislain.gny@gmail.com>
2024-07-29 18:18:05 +0700

    refractor: dockerfile including vite + react + port change in client deployment file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     react({
       devTarget: "es2018",
     }),
   ],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
   build: {
     outDir: "build",
   },
